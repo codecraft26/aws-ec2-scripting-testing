2bf53ba2fa053c5197d21de514fb765c52e604c1
Aman Kumar Gupta <75511608+codecraft26@users.noreply.github.com>
2024-09-26 21:14:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app=express();
 const port=3000;
 
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    res.json({
+        message: "serveris running",
+        "Status": 200
+    });
 }
 );
 
